308a9f472b5ecc0c975caa2878227ad4ea174781
Alveiro <alveiroceron@hotmail.com>
2022-12-06 01:01:05 -0500

    Add standard event pattern

--- Challenge/JsonFileReader.cs
+++ Challenge/JsonFileReader.cs
@@ -1,13 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using Challenge.Models;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Challenge
 {
-    public delegate void Notify<T>(List<T> students);
+    public class FileReadEventArgs<T> : EventArgs
+    {
+        public  List<T> Students { get; set; }
+
+        public FileReadEventArgs(List<T> students)
+        {
+            Students = students;
+        }
+    }
 
     public class JsonFileReader<T>
     {
-        public event Notify<T> OnFileRead;
+        public event EventHandler<FileReadEventArgs<T>> FileRead;
+
+        protected virtual void OnFileRead(FileReadEventArgs<T> e)
+        {
+            FileRead?.Invoke(this, e);
+        }
+
         public void ReadJsonFile(string filePath)
         {
 
@@ -15,7 +30,7 @@ namespace Challenge
             {
                 var json = stReader.ReadToEnd();
                 List<T> elements = Newtonsoft.Json.JsonConvert.DeserializeObject<List<T>>(json);
-                OnFileRead?.Invoke(elements);
+                OnFileRead(new FileReadEventArgs<T>(elements));
 
             }
         }
--- Challenge/Program.cs
+++ Challenge/Program.cs
@@ -1,20 +1,37 @@
-﻿using Challenge;
+﻿namespace Challenge;
 
-Console.WriteLine("1. CollectionMapper");
-Console.WriteLine("2. Linq and Events");
-Console.WriteLine();
-
-int selection = Convert.ToInt16(Console.ReadLine());
-Console.WriteLine();
-
-switch (selection)
+class Program
+{
+    static void Main(string[] args)
     {
-        case 1:
-            Converter.ConverterElements();
-            break;
-        case 2:
-            ProcessFile.Read();
-            break;
-        default:
-            break;
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("CSHARP CHALLENGE!!");
+        Console.WriteLine("MENU:");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("1. CollectionMapper");
+        Console.WriteLine("2. Linq and Events");
+
+        while (true)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Select an option:");
+            
+            int selection = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine();
+
+            switch (selection)
+            {
+                case 1:                   
+                    Converter.ConverterElements();
+                    break;
+                case 2:                   
+                    ProcessFile.Read();
+                    break;
+                default:
+                    break;
+            }
+        }
     }
+}
+
+
